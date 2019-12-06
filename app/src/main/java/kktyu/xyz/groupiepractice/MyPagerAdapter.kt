4764bc34d0ab337626b244fb476ea42e92bb677d
kktyu <ku.k669500@gmail.com>
2019-12-06 19:22:41 +0900

    Raw RecyclerViewの実装追加

--- app/src/main/java/kktyu/xyz/groupiepractice/MyPagerAdapter.kt
+++ app/src/main/java/kktyu/xyz/groupiepractice/MyPagerAdapter.kt
@@ -3,13 +3,17 @@ package kktyu.xyz.groupiepractice
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
+import kktyu.xyz.groupiepractice.rawRecyclerView.RawRecyclerViewFragment
 
 class MyPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(
     fm,
     BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT
 ) {
     override fun getItem(position: Int): Fragment {
-        return Fragment()
+        return when (position) {
+            0 -> RawRecyclerViewFragment()
+            else -> Fragment()
+        }
     }
 
     override fun getCount(): Int {
