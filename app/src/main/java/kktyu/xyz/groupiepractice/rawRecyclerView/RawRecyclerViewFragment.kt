bbec049f7594d39fc63d04e0f09979e5b07ce667
kktyu <ku.k669500@gmail.com>
2019-12-07 23:33:30 +0900

    MVVMに修正

--- app/src/main/java/kktyu/xyz/groupiepractice/rawRecyclerView/RawRecyclerViewFragment.kt
+++ app/src/main/java/kktyu/xyz/groupiepractice/rawRecyclerView/RawRecyclerViewFragment.kt
@@ -5,24 +5,28 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import kktyu.xyz.groupiepractice.R
 import kktyu.xyz.groupiepractice.ViewList
-import kotlinx.android.synthetic.main.fragment_recycler_view.*
+import kktyu.xyz.groupiepractice.databinding.FragmentRecyclerViewBinding
 
 class RawRecyclerViewFragment : Fragment() {
+    lateinit var binding: FragmentRecyclerViewBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_recycler_view, container, false)
+        binding = FragmentRecyclerViewBinding.inflate(inflater, container, false)
+
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        recycler_view.adapter =
+        binding.viewModel = ViewModel(
             ViewAdapter(
                 ViewList().list
             )
+        )
     }
 }
